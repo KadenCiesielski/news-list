ce7e3a894c4e2605b360646da496642c3fecc5e7
Kaden Ciesielski <ciesielskik@merrimack.edu>
2025-04-30 22:15:57 -0400

    removed eslint comments

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -11,13 +11,10 @@ if (!process.env.MONGODB_URI) {
 }
 
 if (process.env.NODE_ENV === "development") {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!(global as any)._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (global as any)._mongoClientPromise = client.connect();
   }
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   clientPromise = (global as any)._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
